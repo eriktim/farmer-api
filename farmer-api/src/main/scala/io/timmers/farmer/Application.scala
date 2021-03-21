5fc9cd2d1d98785b1d46b84c5201ab9f15af1fca
Erik Timmers <e.timmers@gmail.com>
2021-03-21 09:04:02 +0100

    Connect to aggregate

--- farmer-api/src/main/scala/io/timmers/farmer/Application.scala
+++ farmer-api/src/main/scala/io/timmers/farmer/Application.scala
@@ -1,20 +1,33 @@
 package io.timmers.farmer
 
+import io.timmers.cqrs.EventStore
+import io.timmers.farmer.aggregate.Farmer
+import io.timmers.farmer.aggregate.Farmer.FarmerCommand.CreateFarmer
+import io.timmers.farmer.aggregate.Farmer.FarmerEvent
+import zhttp.http.HttpContent._
 import zhttp.http._
 import zhttp.service.Server
 
 import zio._
 
 object Application extends App {
-  val app: Http[Any, HttpError, Request, Response] = Http.collect[Request] {
-    case Method.GET -> Root / "text" =>
-      Response.text("Hello World!")
-  }
+  val app: Http[ZEnv with EventStore.EventStore[FarmerEvent], HttpError, Request, Response] =
+    Http.collectM[Request] { case req @ Method.POST -> Root / "createFarmer" =>
+      req.data.content match {
+        case Empty => ZIO.fail(HttpError.BadRequest("missing payload"))
+        case Complete(data) =>
+          Farmer.aggregate
+            .sendCommand(CreateFarmer(data))
+            .bimap[HttpError, Response](err => HttpError.BadRequest(err), _ => Response.ok)
+        case Chunked(_) => ZIO.fail(HttpError.NotImplemented("chunked"))
+      }
+    }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     system
       .envOrElse("PORT", "8080")
       .map(_.toInt)
       .flatMap(port => Server.start(port, app))
+      .provideCustomLayer(EventStore.inMemory[FarmerEvent])
       .exitCode
 }
