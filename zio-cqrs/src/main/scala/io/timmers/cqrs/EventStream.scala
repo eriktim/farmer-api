cbeca055d03ec059c1c62258b813aac91f067bea
Erik Timmers <e.timmers@gmail.com>
2021-03-27 15:33:43 +0100

    TODO

--- zio-cqrs/src/main/scala/io/timmers/cqrs/EventStream.scala
+++ zio-cqrs/src/main/scala/io/timmers/cqrs/EventStream.scala
@@ -1,7 +1,16 @@
 package io.timmers.cqrs
 
 import zio.stream.ZStream
+import zio.{ Has, Tag }
 
-trait EventStream[P <: Event.Payload] {
-  def subscribe(): ZStream[Any, String, Event[P]]
+object EventStream {
+  type EventStream[P <: Event.Payload] = Has[EventStream.Service[P]]
+
+  trait Service[P <: Event.Payload] {
+    def subscribe(): ZStream[Any, String, Event[P]]
+  }
+
+  def subscribe[P <: Event.Payload: Tag](): ZStream[EventStream[P], String, Event[P]] =
+    ZStream.accessM(_.get.subscribe())
+
 }
--- zio-cqrs/src/main/scala/io/timmers/cqrs/QueryBus.scala
+++ zio-cqrs/src/main/scala/io/timmers/cqrs/QueryBus.scala
@@ -1,6 +1,7 @@
 package io.timmers.cqrs
 
 import io.timmers.cqrs.Event.Payload
+import io.timmers.cqrs.EventStream.EventStream
 import zio.ZIO
 
 trait QueryBus[R, E, Q, S] {
@@ -8,5 +9,11 @@ trait QueryBus[R, E, Q, S] {
 }
 
 object QueryBus {
-  def fromProjection[P <: Payload](): QueryBus[EventStream[P], String, String, String] = ???
+  def fromProjection[P <: Payload](): QueryBus[EventStream[P], String, String, String] =
+    new QueryBus[EventStream[P], String, String, String] {
+      override def read(query: String): ZIO[EventStream[P], String, String] =
+        for {
+          result <- EventStream.subscribe[P]().fold("TODO")((s, _) => s)
+        } yield result
+    }
 }
